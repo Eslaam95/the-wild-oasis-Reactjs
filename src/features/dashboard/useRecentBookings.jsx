ceb8567464e5983c98adbe4498efa303ccce44d7
eslaam95 <eslaam95m@gmail.com>
2023-09-03 13:41:47 +0200

    update default dashboards filter durations

--- src/features/dashboard/useRecentBookings.jsx
+++ src/features/dashboard/useRecentBookings.jsx
@@ -7,7 +7,7 @@ export function useRecentBookings() {
   const [searchParams] = useSearchParams();
 
   const numDays = !searchParams.get("last")
-    ? 7
+    ? 90
     : Number(searchParams.get("last"));
 
   const queryDate = subDays(new Date(), numDays).toISOString();
